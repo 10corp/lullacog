6cf97384adf00da45613cd35c5a0d44d1565b64c
jsansbury <jsansbury@cf9c6dba-3751-4d6f-9fb8-a558297be3c3>
2010-04-18 04:29:56 +0000

    Put 'search' text back in the search box on blur()

--- scripts/lullacog.js
+++ scripts/lullacog.js
@@ -1,6 +1,10 @@
 Drupal.behaviors.lullaCog = function(context) {
   //Add search text to search box
-  $('#search-block-form input.form-text, #search input.form-text', context).val('Search').one('focus', function() {
+  $('#search-block-form input.form-text, #search input.form-text', context).val('Search').focus(function() {
     $(this).val('').addClass('active');
+  }).blur(function() {
+    if ($(this).val() == '') {
+      $(this).val('Search').removeClass('active');
+    }
   });
 };
